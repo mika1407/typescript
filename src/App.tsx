fc0674feecb075accd1e9225d51bd305fbfc680b
mika tiihonen <koneautomaatio@hotmail.com>
2022-04-05 14:56:26 +0300

    todolist lisätty

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 import { Todo } from './model';
 
 const App: React.FC = () => {
@@ -16,12 +17,13 @@ const App: React.FC = () => {
     }
   };
 
-  console.log(todo);
+  console.log(todos);
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/> 
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
